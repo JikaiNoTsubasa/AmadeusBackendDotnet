51ab1cf8675b6527d1a1ca1c79cdaad0ef2e9fe2
Stéphane Biehler <biehler@s2i-evolution.eu>
2024-10-28 16:23:58 +0100

    ✨ Added log4net and cancelled browser opening
    Added log4net and cancelled browser opening

--- ama-back-api/Controllers/JobControllers/LoginController.cs
+++ ama-back-api/Controllers/JobControllers/LoginController.cs
@@ -2,6 +2,7 @@ using System;
 using ama_back_api.Database;
 using ama_back_api.DBModels;
 using ama_back_api.DTO;
+using log4net;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ama_back_api.Controllers.JobControllers;
@@ -9,20 +10,25 @@ namespace ama_back_api.Controllers.JobControllers;
 [ApiController]
 public class LoginController(AmaDBContext context) : AmaController(context)
 {
+    private static readonly ILog log = LogManager.GetLogger(typeof(LoginController));
+    
     [HttpPost]
     [Route("/auth/login")]
     public IActionResult Login([FromBody] RequestLogin model)
     {
         try
         {
+            log.Info($"Login request from {model.Login}");
             AmaUser? user = _context.Users.SingleOrDefault(u => u.Login == model.Login && u.Password == model.Password);
             if (user is null)
             {
+                log.Error($"Login failed for {model.Login}");
                 return StatusCode(StatusCodes.Status401Unauthorized, "Invalid username or password");
             }
             return StatusCode(StatusCodes.Status200OK, user.ToDTO());
         }catch(Exception e)
         {
+            log.Error($"Login failed for {model.Login}: "+e.Message);
             return StatusCode(StatusCodes.Status500InternalServerError, $"Failed to login user {model.Login}."+e.Message);
         }
     }
--- ama-back-api/Program.cs
+++ ama-back-api/Program.cs
@@ -1,15 +1,22 @@
 using ama_back_api.Database;
 using ama_back_api.Global;
+using log4net;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddLog4Net();
+
+ILog log = LogManager.GetLogger(typeof(Program));
 
 builder.Services.AddDbContext<AmaDBContext>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 builder.Services.AddControllersWithViews(
     o=>o.ModelBinderProviders.Insert(0, new FromJsonBinderProvider())
 );
+
 
 var app = builder.Build();
 
@@ -21,10 +28,13 @@ app.UseCors(x => x
         );
 
 // Configure the HTTP request pipeline.
+/*
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
 app.MapControllers();
+
+log.Info("Amadeus API started");
 app.Run();
